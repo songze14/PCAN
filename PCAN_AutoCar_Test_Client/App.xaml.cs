b4b4ceeed442c515548260cf9f5a69b7605d19c7
Administrator <Administrator@GLBMOB-6077>
2025-10-17 16:53:49 +0800

    智能割草机上位机构建

---
+++ PCAN_AutoCar_Test_Client/App.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+using ReactiveUI;
+using Serilog;
+using Splat.Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PCAN_AutoCar_Test_Client
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly Mutex _singletonMutex;
+        public IHost _host { get; private set; }
+        public IServiceProvider RootServiceProvider { get; internal set; }
+        private CancellationTokenSource cts = new CancellationTokenSource();
+        public App()
+        {
+            var appname = typeof(App).AssemblyQualifiedName;
+            this._singletonMutex = new Mutex(true, appname, out var createdNew);
+            InitHost();
+        }
+        private void InitHost()
+        {
+            this._host = Host.CreateDefaultBuilder()
+                .ConfigureAppConfiguration((context, builder) =>
+                {
+                    builder
+                        .SetBasePath(context.HostingEnvironment.ContentRootPath)
+                        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                    builder.AddEnvironmentVariables();
+                })
+                .UseSerilog((hostingContext, services, loggerConfiguration) =>
+                    loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration)
+                )
+                .ConfigureServices(HostStartup.ConfigureServices)
+                .Build();
+            this.RootServiceProvider = this._host.Services;
+            RootServiceProvider.UseMicrosoftDependencyResolver();
+        }
+        private async void OnStartup(object sender, StartupEventArgs e)
+        {
+            RenderOptions.ProcessRenderMode = System.Windows.Interop.RenderMode.SoftwareOnly;
+            try
+            {
+                //var mainWin = this.RootServiceProvider.GetRequiredService<IViewFor<MainWindowViewModel>>() as Window;
+                //mainWin.Show();
+                //var thread = new Thread(async () =>
+                //{
+                //    try
+                //    {
+                //        await _host.RunAsync(cts.Token);
+                //    }
+                //    catch (Exception ex)
+                //    {
+                //        MessageBox.Show(ex.Message);
+                //        this.Shutdown();
+                //    }
+                //});
+                //thread.Start();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                using (var scope = this.RootServiceProvider.CreateScope())
+                {
+                    var sp = scope.ServiceProvider;
+                    var logger = sp.GetRequiredService<ILogger<App>>();
+                    logger.LogError($"{ex.Message}\r\n{ex.StackTrace}");
+                }
+                this.Shutdown();
+            }
+
+        }
+    }
+
+}
---
+++ PCAN_AutoCar_Test_Client/HostStartup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using PCAN.Notification.Log;
+
+using ReactiveUI;
+using Splat;
+using Splat.Microsoft.Extensions.DependencyInjection;
+
+namespace PCAN_AutoCar_Test_Client
+{
+    internal static class HostStartup
+    {
+        public static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.UseMicrosoftDependencyResolver();
+            var resolver = Locator.CurrentMutable;
+            resolver.InitializeSplat();
+            resolver.InitializeReactiveUI();
+          
+
+
+
+
+        }
+
+      
+    }
+}
